e1215101e8a278f40b3fe48c9223ece836761afd
jillp79 <jillplansky@gmail.com>
2021-01-27 00:11:44 -0600

    Testing complete

--- todo-app/src/About/About.js
+++ todo-app/src/About/About.js
@@ -2,14 +2,17 @@ import React, {} from 'react';
 
 function About(props) {
     return (
-        <div>
-            <p>
+
+        <div class="md:container md:mx-auto" style={{display:'flex'}}>
+
+            <p id='aboutPar' style={{alignContent:'center'}}>
             Red Thicket is a small rabbitry located in central Wisconsin specializing in Holland Lops, Satins, Mini Satins and Cinnamons.
             Our rabbitry, well it happened almost by accident. We always kept a rabbit or two as pets. Then my girl's entered 4-H. 
             I thought I'd start a small breeding program to help the girls get some nice show bunnies. One thing lead to another and the girls' interests changed. 
             So Mom's rogue 4-H project, turned hobby and now a growing business; Red Thicket Rabbitry was established.
             </p>
         </div>
+
     );
 }
 
--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Nav from "./Nav";
-
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
-    Link
+    Route
 } from "react-router-dom";
 import About from "./About/About";
 import Todos from "./Todos/Todos";
@@ -13,27 +11,25 @@ import Contact from "./Contacts/Contact";
 import logo from './RedThicketlogo.png';
 
 function App() {
-  return (
-      <Router>
-          <Nav />
-          <header className="App-header">
+    return (
+        <Router>
+            <Nav />
+            <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Switch>
-              <Route path="/about">
-                  <About />
-              </Route>
-              <Route path="/todos">
-                  <Todos />
-              </Route>
-              <Route path="/contact">
-                  <Contact />
-              </Route>
-          </Switch>
-          
-      </Router>
-      
-  );
+            </header>
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/todos">
+                    <Todos />
+                </Route>
+                <Route path="/contact">
+                    <Contact />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
