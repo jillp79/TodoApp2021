7b310aeedaa3d95cea1c8102e0793437a032b57e
jillp79 <jillplansky@gmail.com>
2021-01-26 23:30:41 -0600

    Create About.js

---
+++ todo-app/src/About/About.js
@@ -0,0 +1,16 @@
+import React, {} from 'react';
+
+function About(props) {
+    return (
+        <div>
+            <p>
+            Red Thicket is a small rabbitry located in central Wisconsin specializing in Holland Lops, Satins, Mini Satins and Cinnamons.
+            Our rabbitry, well it happened almost by accident. We always kept a rabbit or two as pets. Then my girl's entered 4-H. 
+            I thought I'd start a small breeding program to help the girls get some nice show bunnies. One thing lead to another and the girls' interests changed. 
+            So Mom's rogue 4-H project, turned hobby and now a growing business; Red Thicket Rabbitry was established.
+            </p>
+        </div>
+    );
+}
+
+export default About;
